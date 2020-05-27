460af6cb82289bd315a2d22580017d294cfd80c5
Michał Regulski <regulskimichal@outlook.com>
2020-05-27 20:35:12 +0200

    CORS configuration

--- src/main/kotlin/pl/pwr/maw/SecurityConfiguration.kt
+++ src/main/kotlin/pl/pwr/maw/SecurityConfiguration.kt
@@ -1,15 +1,20 @@
 package pl.pwr.maw
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
+            .cors()
     }
 
     override fun configure(web: WebSecurity) {
@@ -20,5 +25,16 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 "/v3/api-docs/**",
                 "/swagger-ui.html/**"
             )
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("http://localhost:4200")
+            allowedMethods = listOf("*")
+        }
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", configuration)
+        }
     }
 }
