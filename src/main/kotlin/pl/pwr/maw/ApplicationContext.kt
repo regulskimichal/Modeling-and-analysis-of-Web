cb69ba1a347048be390eded387a742f9f8a821a2
Michał Regulski <regulskimichal@outlook.com>
2020-05-27 21:09:12 +0200

    CORS configuration

--- src/main/kotlin/pl/pwr/maw/ApplicationContext.kt
+++ src/main/kotlin/pl/pwr/maw/ApplicationContext.kt
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Configuration
 @EnableScheduling
@@ -21,6 +24,15 @@ class ApplicationContext {
     fun customOpenAPI(): OpenAPI {
         return OpenAPI()
             .info(Info().title("Page Measurer API").version("1"))
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+            }
+        }
     }
 
 }
--- src/main/kotlin/pl/pwr/maw/SecurityConfiguration.kt
+++ src/main/kotlin/pl/pwr/maw/SecurityConfiguration.kt
@@ -1,20 +1,15 @@
 package pl.pwr.maw
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
-            .cors()
     }
 
     override fun configure(web: WebSecurity) {
@@ -27,14 +22,4 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
             )
     }
 
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource? {
-        val configuration = CorsConfiguration().apply {
-            allowedOrigins = listOf("http://localhost:4200")
-            allowedMethods = listOf("*")
-        }
-        return UrlBasedCorsConfigurationSource().apply {
-            registerCorsConfiguration("/**", configuration)
-        }
-    }
 }
