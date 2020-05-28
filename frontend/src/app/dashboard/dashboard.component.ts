3316c4fa3ee5a1f95ea6513590b1b9526611f55d
ilaydagurbak <ilaydagurbak@gmail.com>
2020-05-28 20:54:32 +0200

    dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -15,16 +15,12 @@ export class DashboardComponent implements OnInit {
         return [
           {title: 'Card 1', cols: 1, rows: 1},
           {title: 'Card 2', cols: 1, rows: 1},
-          {title: 'Card 3', cols: 1, rows: 1},
-          {title: 'Card 4', cols: 1, rows: 1}
         ];
       }
 
       return [
         {title: 'Card 1', cols: 2, rows: 1},
-        {title: 'Card 2', cols: 1, rows: 1},
-        {title: 'Card 3', cols: 1, rows: 2},
-        {title: 'Card 4', cols: 1, rows: 1}
+        {title: 'Card 2', cols: 2, rows: 1},
       ];
     })
   );
@@ -34,6 +30,7 @@ export class DashboardComponent implements OnInit {
     private apiService: ApiService
   ) {
   }
+
 
   async ngOnInit() {
     const apis = await this.apiService.getApis();
